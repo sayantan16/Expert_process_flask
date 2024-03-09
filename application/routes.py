bfc13a8de6d334b08710b83e383e0411011c1d23
sayantan16 <sayantankundu1611@gmail.com>
2024-03-09 16:20:06 -0500

    initial changes for the code block

--- application/routes.py
+++ application/routes.py
@@ -7,3 +7,15 @@ from flask import render_template
 @app.route("/#")
 def index():
     return render_template("index.html")
+
+@app.route("/code")
+def code():
+    return render_template("code.html")
+
+@app.route("/math")
+def math():
+    return render_template("math.html")
+
+@app.route("/vis")
+def vis():
+    return render_template("vis.html")
