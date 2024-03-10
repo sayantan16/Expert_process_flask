710e74e45eb7f3cfa35c4ba46705f73ce73e8b24
sayantan16 <sayantankundu1611@gmail.com>
2024-03-10 10:51:11 -0400

    Fixed the dropdown button in nav bar

--- application/routes.py
+++ application/routes.py
@@ -1,21 +1,21 @@
 from application import app
-from flask import render_template
+from flask import render_template, jsonify, request
 
 
 @app.route("/")
 @app.route("/index")
 @app.route("/#")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title='Choose a Problem')
 
 @app.route("/code")
 def code():
-    return render_template("code.html")
+    return render_template("code.html", page_title='Coding Problem')
 
 @app.route("/math")
 def math():
-    return render_template("math.html")
+    return render_template("math.html", page_title='Math Problem')
 
 @app.route("/vis")
 def vis():
-    return render_template("vis.html")
+    return render_template("vis.html", page_title='Visualization Problem')
